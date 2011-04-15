1da6bb07b3c2d31f48875fa8d29e5598cf99ffa3
Gazal Garg <gazal24@gmail.com>
2011-04-16 03:51:53 +0530

    changes done in USER MODULE regarding friend request acceptance and rejection

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,8 @@ class UserController < ApplicationController
 
   #THIS IS TO VIEW ANY USERS PROFILE. PUT ONLY SPECIFIC DETAILS HERE
   def show
+    redirect_to :action => 'showme' if params[:id].to_s == session[:user].id.to_s
+
     @user = User.find(params[:id])
 
     #this is for searching the existing friends of this use
@@ -13,6 +15,10 @@ class UserController < ApplicationController
     @friends = @friends1 + @friends2
     
     @albums = @user.albums
+    
+    # @isFriend = Friend.find(:all, :conditions => ["(user1 = #{session[:user].id} and user2 = #{params[:id]})"]).first
+    @FriendRequestSend = Friend.find(:all, :conditions => ["(user1 = #{session[:user].id} and user2 = #{params[:id]} and accepted = 0)"]).first
+    @FriendRequestReceived = Friend.find(:all, :conditions => ["(user1 = #{params[:id]} and user2 = #{session[:user].id}  and accepted = 0)"]).first        
   end
   
   #THIS IS MY PROFILE HOMEPAGE. PUT ALL THE IMPORTANT DETAILS HERE
@@ -34,35 +40,15 @@ class UserController < ApplicationController
     @search = User.find_all_by_name(params[:query]["name"])
   end
   
-  def follow
-    @f = Friend.new(:user1 => session[:user].id, :user2 => params[:id], :accepted => 0)
-    if @f.save
-      redirect_to :action => 'show', :id => session[:user].id
-    else
-      redirect_to :action => 'search'
-    end
-  end
 
-  def accept
-    @friend = Friend.all(:conditions => {:user1 => params[:id], :user2 => session[:user].id, :accepted => 0})
-    @friend = Friend.find(@friend.to_param.to_i)
-    if !@friend.blank?
-      @friend.accepted = 1
-      if @friend.save
-        redirect_to :action => 'show', :id => session[:user].id
-      else
-        redirect_to :action => 'search'
-      end
-    end
-  end
-  
   def create
     @user = User.new(params[:user])
     if @user.save
       session[:user] = User.authenticate(@user.email, @user.password)
-      #      flash[:message] = "Signup successful"
-      redirect_to :action => 'show', :id => session[:user].id
+      flash[:notice] = "Signup successful"
+      redirect_to :action => 'showme'
     else
+      flash[:error] = "Something is wrong"
       redirect_to :controller => 'homepage', :action => 'index'
     end
   end
@@ -70,14 +56,21 @@ class UserController < ApplicationController
   def login
     @user = User.authenticate(params[:user][:email], params[:user][:password])
     if @user.blank?
+      flash[:error] = "Wrong Email-Password"
       redirect_to :action => 'index', :controller => 'homepage'
     else
-      # flash[:message]  = "Login successful"
+      flash[:success]  = "Login successful"
       session[:user] = @user
       redirect_to :action => 'showme', :controller => 'user'
     end
   end
   
+  def logout
+    session[:user] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to :action => 'index', :controller => 'homepage'
+  end
+
   def edit
     @user = session[:user]
   end
@@ -94,6 +87,43 @@ class UserController < ApplicationController
   def delete
     Book.find(params[:id]).destroy
     redirect_to :action => 'list'
+  end
+
+  def follow
+    @f = Friend.new(:user1 => session[:user].id, :user2 => params[:id], :accepted => 0)
+    if @f.save
+      flash[:notice] = "Follow request send to #{User.find(params[:id]).name}"
+      redirect_to :action => 'showme'
+    else
+      redirect_to :action => 'search'
+    end
+  end
+
+  def accept
+    @f = Friend.find(params[:id])
+    @f.accepted = 1
+    @f.save
+    flash[:success] = "Accepted"
+    redirect_to :action => 'showme', :controller => 'user'
+#     @friend = Friend.all(:conditions => {:user1 => params[:id], :user2 => session[:user].id, :accepted => 0})
+#     @friend = Friend.find(@friend.to_param.to_i)
+#     if !@friend.blank?
+#       @friend.accepted = 1
+#       if @friend.save
+#         redirect_to :action => 'show', :id => session[:user].id
+#       else
+#         redirect_to :action => 'search'
+#       end
+#     end
+  end
+  
+  def unfollow
+    @f = Friend.find(params[:id])
+    user2 = @f.user2
+    @f.destroy
+    @f.save
+    redirect_to :action => 'show', :controller => 'user', :id => user2 
+    flash[:notice] = "Follow request cancel"
   end
   
   #======== L O G I N =======================
@@ -113,12 +143,7 @@ class UserController < ApplicationController
   end
   
 
-  def logout
-    session[:user] = nil
-    flash[:message] = 'Logged out'
-    redirect_to :action => 'login'
-  end
-
+  
   def forgot_password
     if request.post?
       u= User.find_by_email(params[:user][:email])
