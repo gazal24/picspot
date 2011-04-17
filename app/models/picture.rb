8776f2fae90f0514478b02e237ecb6337af144e4
Gazal Garg <gazal24@gmail.com>
2011-04-17 14:28:46 +0530

    after scaffolding of pictures and paperclip generate of photo in pictues

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,12 +1,9 @@
 require 'paperclip'
-
 class Picture < ActiveRecord::Base
-  belongs_to :album
-  belongs_to :user
-  has_many :comments
   has_attached_file :photo,
-  :path => ':rails_root/public/system/:id.:extension',
   :styles => {
-    :thumb=> "100x100#",
-    :small  => "150x150>" }
+    :thumb => "75x75>",
+    :small => "150x150>"
+  }
+  
 end
